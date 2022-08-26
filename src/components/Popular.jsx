c7fdbe9fbe081b684f7e45063a2b09bfca6d8062
Suresh Shrestha <sureshshrestha@mail.weber.edu>
2022-08-26 00:34:43 -0600

    fetch random recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+
 function Popular() {
-  return (
-    <div>Popular</div>
-  )
+
+    const [popular, setPopular] = useState([]);
+
+    useEffect(() => {
+        getPopular();
+    } , []);
+
+    const getPopular = async () => {
+
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+        const data = await api.json();
+        setPopular(data.recipes);
+        console.log(data);
+    }
+
+    return (
+    
+        <div>
+            {popular.map((recipe) => {
+                return (
+                <div key={recipe.id}>
+                    <p>{recipe.title}</p>
+                </div>
+                );
+            })}
+            
+        </div>
+    );
 }
 
 export default Popular
