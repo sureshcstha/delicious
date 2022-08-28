9a285d6a1e548822334a6cc9752a80f7c1be0d69
Suresh Shrestha <sureshshrestha@mail.weber.edu>
2022-08-27 18:25:49 -0600

    Search bar and search result page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter, Link } from 'react-router-dom';
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
+      <Search />
       <Category />
       <Pages />
     </BrowserRouter>
