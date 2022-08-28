b0bf947bfd1a5ee4534e7eef4efacaf016e34bcc
Suresh Shrestha <sureshshrestha@mail.weber.edu>
2022-08-27 18:08:17 -0600

    add category

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
+      <Category />
       <Pages />
+    </BrowserRouter>
     </div>
   );
 }
