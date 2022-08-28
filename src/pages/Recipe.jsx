2b5c5f0c8fcda40379b4905ec602dcfbb92e0142
Suresh Shrestha <sureshshrestha@mail.weber.edu>
2022-08-27 20:13:57 -0600

    fix error: React Hook useEffect has a missing dependency

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -7,14 +7,14 @@ function Recipe() {
   const [activeTab, setActiveTab] = useState('instructions');
   let params = useParams();
 
-  const fetchDetails = async () => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
-    const detailData = await data.json();
-    setDetails(detailData);
-    console.log(detailData);
-  };
-
   useEffect(() => {
+    const fetchDetails = async () => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
+        const detailData = await data.json();
+        setDetails(detailData);
+        console.log(detailData);
+    };
+    
     fetchDetails();
   }, [params.name]);
 
